df733c1412ee4b1a803dc3dfdd61ee7a7dc9a451
Peyman <118217228+Ptechgithub@users.noreply.github.com>
2023-09-03 13:30:50 +0330

    update hysteria2.sh

--- hy2/hysteria2.sh
+++ hy2/hysteria2.sh
@@ -396,7 +396,6 @@ changeport(){
 
     green "Hysteria 2 port successfully modified to: $port"
     yellow "Please manually update the client configuration file to use node"
-    showconf
 }
 
 changepasswd(){
